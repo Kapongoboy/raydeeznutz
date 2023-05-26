8c53ebf6d124ab2df0aa0ff7b77e906926e0d23d
Plamedi Luis <plamedi.luis11@gmail.com>
2023-05-26 20:01:55 +0200

    Tried and failed to get it working

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include "raylib.h"
 
+#define MAX_BUILDINGS 100
+
 void check_user_key(Vector2 &position) {
         if (IsKeyDown(KEY_A)) {
             position.x -= 2.0f;
@@ -12,6 +14,43 @@ void check_user_key(Vector2 &position) {
         }
         if (IsKeyDown(KEY_W)) {
             position.y -= 2.0f;
+        }
+}
+
+void move_player(Rectangle &player, Camera2D &camera) {
+        if (IsKeyDown(KEY_A)) {
+            player.x -= 2.0f;
+        } else if (IsKeyDown(KEY_D)) {
+            player.x += 2.0f;
+        }
+
+        camera.target = (Vector2){
+            player.x + 20, player.y + 20
+        };
+
+        if (IsKeyDown(KEY_Q)) {
+            camera.rotation--;
+        } else if (IsKeyDown(KEY_E)) {
+            camera.rotation++;
+        }
+
+        if (camera.rotation > 40) {
+            camera.rotation = 40;
+        } else if (camera.rotation < - 40) {
+            camera.rotation = -40;
+        }
+
+        camera.zoom += ((float)GetMouseWheelMove()*0.05f);
+
+        if (camera.zoom > 3.0f) {
+            camera.zoom = 3.0f;
+        } else if (camera.zoom < 0.1f) {
+            camera.zoom = 0.1f;
+        }
+
+        if (IsKeyPressed(KEY_R)) {
+            camera.zoom = 1.0f;
+            camera.rotation = 0.0f;
         }
 }
 
@@ -40,17 +79,81 @@ int main(void) {
     };
 
     Color ballColor = DARKBLUE;
+    Rectangle player = {400, 280, 40, 40};
+    Rectangle buildings[MAX_BUILDINGS] = { 0 };
+    Color buildColors[MAX_BUILDINGS] = {0};
+
+    int spacing = 0;
+    
+    for (int i = 0; i < MAX_BUILDINGS; i++) {
+        buildings[i].width = (float)GetRandomValue(50, 200);
+        buildings[i].height = (float)GetRandomValue(100, 800);
+        buildings[i].y = screenHeight - 130.0f - buildings[i].height;
+        buildings[i].x = -6000.0f + spacing;
+
+        spacing += (int)buildings[i].width;
+
+        buildColors[i] = (Color){
+            GetRandomValue(200, 240),
+            GetRandomValue(200, 240),
+            GetRandomValue(200, 240),
+        };
+
+    }
+
+    Camera2D camera = {0};
+    camera.target = (Vector2){player.x + 20.0f, player.y + 20.0f};
+    camera.offset = (Vector2){screenWidth/2.0f, screenHeight/2.0f};
+    camera.rotation = 0.0f;
+    camera.zoom = 1.0f;
 
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
-        check_user_key(ballPosition);
-        check_user_mouse(ballPosition, ballColor);
+        // check_user_key(ballPosition);
+        // check_user_mouse(ballPosition, ballColor);
+        move_player(player, camera);
 
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Ordained the game for real this time", 10, 10, 20, DARKPURPLE);
-        DrawCircleV(ballPosition, 50, ballColor);
+
+            ClearBackground(RAYWHITE);
+
+            BeginMode2D(camera);
+                DrawRectangle(-6000, 320, 13000, 8000, DARKGRAY);
+                for (int i = 0; i < MAX_BUILDINGS; i++) {
+                    DrawRectangleRec(
+                            buildings[i],
+                            buildColors[i]
+                            );
+                }
+                DrawRectangleRec(player, RED);
+                DrawLine(
+                        (int)camera.target.x,
+                        -screenHeight*10,
+                        (int)camera.target.x,
+                        screenWidth*10,
+                        GREEN
+                        );
+                DrawLine(
+                        -screenWidth*10,
+                        (int)camera.target.y,
+                        screenWidth*10,
+                        (int)camera.target.y,
+                        GREEN
+                        );
+            EndMode2D();
+
+            DrawText(
+                    "Ordained the game for real this time",
+                    640, 10, 20, RED);
+            DrawRectangle(0, 0, screenWidth, 5, RED);
+            DrawRectangle(0, 5, 5, screenHeight, RED);
+            DrawRectangle(screenWidth - 5, 5, 5, screenHeight - 10, RED);
+            DrawRectangle(0, screenHeight - 5, screenWidth, 5, RED);
+
+            DrawRectangle(10, 10, 250, 113, Fade(RED, 0.5f));
+            DrawRectangleLines(10, 10, 250, 113, BLUE);
+
         EndDrawing();
     }
 
