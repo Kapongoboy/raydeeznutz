b2aef48b7679748775626a28e702edd3607a95eb
Plamedi Luis <plamedi.luis11@gmail.com>
2023-05-26 14:12:35 +0200

    mouse with color changing example

--- rustbtw/src/main.rs
+++ rustbtw/src/main.rs
@@ -1,5 +1,37 @@
 use raylib::prelude::*;
 
+fn check_user_key(position: &mut Vector2, rl: &mut RaylibHandle) -> () {
+        if rl.is_key_down(KeyboardKey::KEY_A) {
+            position.x -= 2.0;
+        }
+        if rl.is_key_down(KeyboardKey::KEY_D) {
+            position.x += 2.0;
+        }
+        if rl.is_key_down(KeyboardKey::KEY_S) {
+            position.y += 2.0;
+        }
+        if rl.is_key_down(KeyboardKey::KEY_W) {
+            position.y -= 2.0;
+        }
+}
+
+fn check_user_mouse(
+    position: &mut Vector2,
+    rl: &mut RaylibHandle,
+    ball_color: &mut Color
+    ) -> () {
+    *position = rl.get_mouse_position();
+
+    if rl.is_mouse_button_pressed(MouseButton::MOUSE_MIDDLE_BUTTON) {
+        *ball_color = Color::MAROON;
+    } else if rl.is_mouse_button_pressed(MouseButton::MOUSE_RIGHT_BUTTON) {
+        *ball_color = Color::LIME;
+    } else if rl.is_mouse_button_pressed(MouseButton::MOUSE_LEFT_BUTTON) {
+        *ball_color = Color::PURPLE;
+    }
+
+}
+
 fn main() {
     let (mut rl, thread) = raylib::init()
         .size(800, 480)
@@ -10,27 +42,18 @@ fn main() {
         rl.get_screen_width() as f32 / 2.0,
         rl.get_screen_height() as f32 / 2.0,);
 
+    let mut ball_color = Color::DARKPURPLE;
+
     rl.set_target_fps(60);
 
     while !rl.window_should_close() {
-        if rl.is_key_down(KeyboardKey::KEY_A) {
-            ball_position.x -= 2.0;
-        }
-        if rl.is_key_down(KeyboardKey::KEY_D) {
-            ball_position.x += 2.0;
-        }
-        if rl.is_key_down(KeyboardKey::KEY_S) {
-            ball_position.y += 2.0;
-        }
-        if rl.is_key_down(KeyboardKey::KEY_W) {
-            ball_position.y -= 2.0;
-        }
-
+        // check_user_key(&mut ball_position, &mut rl);
+        check_user_mouse(&mut ball_position, &mut rl, &mut ball_color);
         let mut d = rl.begin_drawing(&thread);
         d.clear_background(Color::RAYWHITE);
         d.draw_text(
             "Ordained the game for real this time",
             10, 10, 20, Color::DARKPURPLE);
-        d.draw_circle_v(ball_position, 50.0, Color::MAROON);
+        d.draw_circle_v(ball_position, 50.0, ball_color);
     }
 }
