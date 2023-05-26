f8459cd98d08f8c3c078721092442462a4bd91c4
Plamedi Luis <plamedi.luis11@gmail.com>
2023-05-26 14:29:48 +0200

    added mouse example function

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,33 @@
 #include "raylib.h"
 
+void check_user_key(Vector2 &position) {
+        if (IsKeyDown(KEY_A)) {
+            position.x -= 2.0f;
+        }
+        if (IsKeyDown(KEY_D)) {
+            position.x += 2.0f;
+        }
+        if (IsKeyDown(KEY_S)) {
+            position.y += 2.0f;
+        }
+        if (IsKeyDown(KEY_W)) {
+            position.y -= 2.0f;
+        }
+}
+
+void check_user_mouse(Vector2 &position, Color &ballColor) {
+    if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+        position = GetMousePosition();
+        ballColor = MAROON;
+    }
+    else if (IsMouseButtonPressed(MOUSE_BUTTON_MIDDLE)) ballColor = LIME;
+    else if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)) ballColor = DARKBLUE;
+    else if (IsMouseButtonPressed(MOUSE_BUTTON_SIDE)) ballColor = PURPLE;
+    else if (IsMouseButtonPressed(MOUSE_BUTTON_EXTRA)) ballColor = YELLOW;
+    else if (IsMouseButtonPressed(MOUSE_BUTTON_FORWARD)) ballColor = ORANGE;
+    else if (IsMouseButtonPressed(MOUSE_BUTTON_BACK)) ballColor = BEIGE;
+}
+
 int main(void) {
     const int screenWidth = 800;
     const int screenHeight = 450;
@@ -11,26 +39,18 @@ int main(void) {
         (float)screenHeight/2
     };
 
+    Color ballColor = DARKBLUE;
+
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
-        if (IsKeyDown(KEY_A)) {
-            ballPosition.x -= 2.0f;
-        }
-        if (IsKeyDown(KEY_D)) {
-            ballPosition.x += 2.0f;
-        }
-        if (IsKeyDown(KEY_S)) {
-            ballPosition.y += 2.0f;
-        }
-        if (IsKeyDown(KEY_W)) {
-            ballPosition.y -= 2.0f;
-        }
+        check_user_key(ballPosition);
+        check_user_mouse(ballPosition, ballColor);
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
         DrawText("Ordained the game for real this time", 10, 10, 20, DARKPURPLE);
-        DrawCircleV(ballPosition, 50, MAROON);
+        DrawCircleV(ballPosition, 50, ballColor);
         EndDrawing();
     }
 
