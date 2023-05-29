53d6b810fc05c51cd0381345441044cd98782526
Plamedi Luis <plamedi.luis11@gmail.com>
2023-05-29 13:59:48 +0200

    building creation functionality moved here

---
+++ include/environment.hpp
@@ -0,0 +1,46 @@
+#pragma once
+#include <iostream>
+#include <raylib.h>
+#define NUM_BUILDINGS 100
+
+struct Buildings {
+    const int h;
+    const int w;
+    int spacing = 0;
+    Rectangle buildings[NUM_BUILDINGS] = {0};
+    Color building_colors[NUM_BUILDINGS] = {0};
+
+    Buildings(const int h, const int w ): h(h), w(w) {
+        for (int i = 0; i < NUM_BUILDINGS; i++) {
+                buildings[i].width = (float)GetRandomValue(50, 200);
+                buildings[i].height = (float)GetRandomValue(100, 800);
+                buildings[i].y = h - 130.0f - buildings[i].height;
+                buildings[i].x = -6000.0f + spacing;
+
+                spacing += (int)buildings[i].width;
+
+                building_colors[i] = (Color){
+                    GetRandomValue(200, 240),
+                    GetRandomValue(200, 240),
+                    GetRandomValue(200, 250),
+                    255 
+                };
+
+            }
+    }
+
+    ~Buildings() {
+        std::cout << "buildings memory freed" << "\n";
+    }
+
+    void draw_buildings() {
+        DrawRectangle(-6000, 320, 13000, 8000, DARKGRAY); // grey base
+
+        for (int i = 0; i < NUM_BUILDINGS; i++) {
+            DrawRectangleRec(
+                    buildings[i],
+                    building_colors[i]
+                    );
+        }
+    }
+};
