57c0f018e63feb48acbf41d20737b1339bb8cef1
Plamedi Luis <plamedi.luis11@gmail.com>
2023-06-10 14:02:52 +0200

    sorted out the main function to ony call game run and setup the config

--- rust_version/src/main.rs
+++ rust_version/src/main.rs
@@ -1,18 +1,26 @@
-use raylib::prelude::*;
-const MAX_BUILDINGS: usize = 100;
+use std::env;
 
 pub mod game;
+pub mod config;
+
+use config::Config;
+
 
 fn main() {
-    use raylib::consts::KeyboardKey::*;
-    let (w, h) = (800, 450);
-    let (mut rl, thread) = raylib::init()
-        .size(w, h)
-        .title("raydeezrust")
-        .build();
-
-    game::run(&mut rl, &thread);
+    let args: Vec<String> = env::args().collect();
+    game::run(parse_config(&args));
 }
 
-
+fn parse_config(args: &[String]) -> Config {
+    if args.len() < 4 {
+        panic!("Not enough arguments");
+    }
+    let (w, h) = (
+        args[1].parse::<i32>().expect("Expected a number for the width"),
+        args[2].parse::<i32>().expect("Expected a number for the height")
+        );
+    let title = &args[3];
+    let config = Config::new(w, h, title.to_string());
+    return config;
+}
 
