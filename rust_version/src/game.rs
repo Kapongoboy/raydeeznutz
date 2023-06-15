acc6f0012c71299093af47163e4a8d1bb7596c8e
Plamedi Luis <plamedi.luis11@gmail.com>
2023-06-16 00:10:51 +0200

    moved buildings to environment file

--- rust_version/src/game.rs
+++ rust_version/src/game.rs
@@ -4,24 +4,6 @@ pub mod character;
 use raylib::prelude::*;
 use crate::config::Config;
 
-const MAX_BUILDINGS: usize = 100;
-
-pub struct Building {
-    size: Vec<Rectangle>,
-    colors: Vec<Color>,
-    spacing: f32,
-}
-
-impl Building {
-    pub fn build() -> Building {
-        return Building {
-            size: Vec::with_capacity(MAX_BUILDINGS),
-            colors: Vec::with_capacity(MAX_BUILDINGS),
-            spacing: 0.0,
-        }
-    }
-}
-
 
 
 
--- rust_version/src/game/environment.rs
+++ rust_version/src/game/environment.rs
@@ -1,1 +1,24 @@
+use raylib::prelude::*;
+
+const MAX_BUILDINGS: usize = 100;
+
+pub struct Building {
+    size: Vec<Rectangle>,
+    colors: Vec<Color>,
+    spacing: f32,
+}
+
+impl Building {
+    pub fn build() -> Building {
+        return Building {
+            size: Vec::with_capacity(MAX_BUILDINGS),
+            colors: Vec::with_capacity(MAX_BUILDINGS),
+            spacing: 0.0,
+        }
+    }
+
+    pub fn draw(building: Building) {
+
+    }
+}
 
